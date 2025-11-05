a30528fd4770735567ed6d4af71a32d183039ef0
mdkhatriS3 <mdkhatriS3@gmail.com>
2025-11-05 14:27:45 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react-swc'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/impact-incidents/',  // 👈 important for GitHub Pages
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  base: '/impact-incidents/', // 👈 this must match your repo name for GitHub Pages
 })

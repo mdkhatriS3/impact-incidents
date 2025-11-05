abb05bdeb3863a3ae8c75fe59394f2943ccb6ba7
mdkhatriS3 <mdkhatriS3@gmail.com>
2025-11-05 13:58:51 +0530

    rechecked plug in import

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,12 @@
 import { defineConfig } from 'vite'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/impact-incidents/', // Required for GitHub Pages
+  base: '/impact-incidents/',       // required for GitHub Pages
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'), // make @ point to /src
+    },
+  },
 })

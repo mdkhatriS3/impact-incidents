0c2d6ca76205a9657f77c33b05f91920d4a9ce93
mdkhatriS3 <mdkhatriS3@gmail.com>
2025-11-05 14:19:47 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,8 @@
 import { defineConfig } from 'vite'
-import path from 'path'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/impact-incidents/',
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
-    },
-  },
+  plugins: [react()],
+  base: '/impact-incidents/',  // 👈 important for GitHub Pages
 })
